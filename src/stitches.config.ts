e5615fbd05e76ea289f3e86e06c9375a49f9062b
Javier Diaz <coderdiaz@gmail.com>
2022-09-19 15:15:26 -0500

    feat: added sans font family to stitches

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -40,6 +40,9 @@ export const { styled, getCssText, globalCss, css, keyframes } = createStitches(
       gray800: '#1f2937',
       gray900: '#111827',
     },
+    fonts: {
+      sans: 'Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
   },
   utils: {
     mx: (value: number) => ({ marginLeft: value, marginRight: value }),
