c61e36ddba84892a9b70431c8ba0ca83b54d66ae
Javier Diaz <coderdiaz@gmail.com>
2022-09-19 15:23:52 -0500

    feat: added required weights from fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -29,7 +29,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=optional"
             rel="stylesheet"
           />
           <style
