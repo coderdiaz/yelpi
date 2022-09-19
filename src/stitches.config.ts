5fab2ace378b707768f0b1103b68c1d313520fa8
Javier Diaz <coderdiaz@gmail.com>
2022-09-18 22:16:22 -0500

    feat: added base stitches config

---
+++ src/stitches.config.ts
@@ -0,0 +1,8 @@
+import { createStitches } from '@stitches/react';
+export type { CSS, VariantProps, CSSProperties } from '@stitches/react';
+
+export const { styled, getCssText, globalCss, css, keyframes } = createStitches({
+  media: {},
+  theme: {},
+  utils: {},
+});
