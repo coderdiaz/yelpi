d2db802570c0ba8461a2d570e415a7dd490d32b6
Javier Diaz <coderdiaz@gmail.com>
2022-09-18 22:28:32 -0500

    feat: added base colors

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -12,6 +12,34 @@ export const { styled, getCssText, globalCss, css, keyframes } = createStitches(
   media: {
     ...breakpoints,
   },
-  theme: {},
+  theme: {
+    colors: {
+      // Base
+      white: '#ffffff',
+      black: '#000000',
+      // Slate
+      slate50: '#f8fafc',
+      slate100: '#f1f5f9',
+      slate200: '#e2e8f0',
+      slate300: '#cbd5e1',
+      slate400: '#94a3b8',
+      slate500: '#64748b',
+      slate600: '#475569',
+      slate700: '#334155',
+      slate800: '#1e293b',
+      slate900: '#0f172a',
+      // Gray
+      gray50: '#f9fafb',
+      gray100: '#f3f4f6',
+      gray200: '#e5e7eb',
+      gray300: '#d1d5db',
+      gray400: '#9ca3af',
+      gray500: '#6b7280',
+      gray600: '#4b5563',
+      gray700: '#1f2937',
+      gray800: '#1f2937',
+      gray900: '#111827',
+    },
+  },
   utils: {},
 });
