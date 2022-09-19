753c20cf4540cb7136ca141c371ec23ea199ba97
Javier Diaz <coderdiaz@gmail.com>
2022-09-18 22:22:42 -0500

    feat: added breakpoints for responsiveness

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -1,8 +1,17 @@
 import { createStitches } from '@stitches/react';
 export type { CSS, VariantProps, CSSProperties } from '@stitches/react';
 
+export const breakpoints = {
+  sm: '(min-width: 640px)',
+  md: '(min-width: 768px)',
+  lg: '(min-width: 1024px)',
+  xl: '(min-width: 1280px)',
+};
+
 export const { styled, getCssText, globalCss, css, keyframes } = createStitches({
-  media: {},
+  media: {
+    ...breakpoints,
+  },
   theme: {},
   utils: {},
 });
