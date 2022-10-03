7c2f39ce4fe4a07376acc3829f30b153f20da674
Javier Diaz <coderdiaz@gmail.com>
2022-10-03 15:23:04 -0500

    feat: added stack primitive component

---
+++ src/components/Stack.tsx
@@ -0,0 +1,54 @@
+import { styled } from '@stitches/config';
+import type { CSS } from '@stitches/config';
+import { createElement } from 'react';
+
+type StackProps = {
+  as?: string;
+  children?: React.ReactNode;
+  direction?: 'column' | 'column-reverse' | 'row' | 'row-reverse';
+  justify?: 'start' | 'end' | 'center' | 'stretch';
+  align?: 'start' | 'end' | 'center' | 'stretch' | 'baseline';
+  css?: CSS;
+}
+
+export default function Stack({
+  as,
+  children,
+  ...props
+}: StackProps) {
+  const StyledStack = styled(as as any, {
+    display: 'flex',
+    variants: {
+      direction: {
+        row: { flexDirection: 'row' },
+        column: { flexDirection: 'column' },
+        'column-reverse': { flexDirection: 'column-reverse' },
+        'row-reverse': { flexDirection: 'row-reverse' },
+      },
+      justify: {
+        start: { justifyContent: 'start' },
+        end: { justifyContent: 'end' },
+        center: { justifyContent: 'center' },
+        stretch: { justifyContent: 'stretch' },
+      },
+      align: {
+        start: { alignItems: 'start' },
+        end: { alignItems: 'end' },
+        center: { alignItems: 'center' },
+        stretch: { alignItems: 'stretch' },
+        baseline: { alignItems: 'baseline' },
+      }
+    },
+    defaultVariants: {
+      direction: 'row',
+      justify: 'start',
+      align: 'start',
+    },
+  });
+
+  return createElement(
+    StyledStack,
+    { ...props },
+    children,
+  );
+}
