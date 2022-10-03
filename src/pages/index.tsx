78262efd0caf2ad9b7dc0f294bcc2f3d275cb339
Javier Diaz <coderdiaz@gmail.com>
2022-10-03 16:33:02 -0500

    feat: improved visual elements in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import { styled } from '@stitches/config';
 import Header from '@components/Header';
+import { Container } from '@components/Container';
+import Sidebar from '@components/Sidebar';
+import BodySection from '@components/BodySection';
 
 export default function IndexPage() {
   return (
     <>
       <Header />
+      <Main>
+        <StyledContainer size="xl">
+          <Sidebar></Sidebar>
+          <Divisor />
+          <BodySection></BodySection>
+        </StyledContainer>
+      </Main>
     </>
   );
 }
+
+const StyledContainer = styled(Container, {
+  display: 'flex',
+  spaceX: 32,
+  height: '100%',
+});
+
+const Divisor = styled('div', {
+  height: '100%',
+  width: 1,
+  background: '$slate300',
+});
+
+const Main = styled('main', {
+  height: 'calc(100vh - 64px)',
+  maxHeight: 'calc(100vh - 64px)',
+});
