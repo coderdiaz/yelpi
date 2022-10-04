f476a1c2405c2200d4519793d2ed4c58c8f5736c
Javier Diaz <coderdiaz@gmail.com>
2022-10-03 23:11:56 -0500

    feat: added support for ref in stack component

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,6 +1,6 @@
+import { createElement, forwardRef } from 'react';
 import { styled } from '@stitches/config';
 import type { CSS } from '@stitches/config';
-import { createElement } from 'react';
 
 type StackProps = {
   children?: React.ReactNode;
@@ -10,43 +10,45 @@ type StackProps = {
   css?: CSS;
 }
 
-export default function Stack({
-  children,
-  ...props
-}: StackProps) {
-  const StyledStack = styled('div', {
-    display: 'flex',
-    variants: {
-      direction: {
-        row: { flexDirection: 'row' },
-        column: { flexDirection: 'column' },
-        'column-reverse': { flexDirection: 'column-reverse' },
-        'row-reverse': { flexDirection: 'row-reverse' },
+const Stack = forwardRef<HTMLDivElement, StackProps>(
+  ({ children, ...props }, forwardedRef) => {
+    const StyledStack = styled('div', {
+      display: 'flex',
+      position: 'relative',
+      variants: {
+        direction: {
+          row: { flexDirection: 'row' },
+          column: { flexDirection: 'column' },
+          'column-reverse': { flexDirection: 'column-reverse' },
+          'row-reverse': { flexDirection: 'row-reverse' },
+        },
+        justify: {
+          start: { justifyContent: 'start' },
+          end: { justifyContent: 'end' },
+          center: { justifyContent: 'center' },
+          stretch: { justifyContent: 'stretch' },
+        },
+        align: {
+          start: { alignItems: 'start' },
+          end: { alignItems: 'end' },
+          center: { alignItems: 'center' },
+          stretch: { alignItems: 'stretch' },
+          baseline: { alignItems: 'baseline' },
+        }
       },
-      justify: {
-        start: { justifyContent: 'start' },
-        end: { justifyContent: 'end' },
-        center: { justifyContent: 'center' },
-        stretch: { justifyContent: 'stretch' },
+      defaultVariants: {
+        direction: 'row',
+        justify: 'start',
+        align: 'start',
       },
-      align: {
-        start: { alignItems: 'start' },
-        end: { alignItems: 'end' },
-        center: { alignItems: 'center' },
-        stretch: { alignItems: 'stretch' },
-        baseline: { alignItems: 'baseline' },
-      }
-    },
-    defaultVariants: {
-      direction: 'row',
-      justify: 'start',
-      align: 'start',
-    },
-  });
+    });
+  
+    return createElement(
+      StyledStack,
+      { ...props, ref: forwardedRef },
+      children,
+    );
+  }
+);
 
-  return createElement(
-    StyledStack,
-    { ...props },
-    children,
-  );
-}
+export default Stack;
