a4453b974f45d1fa4425f8d2eee0064402f7a936
Javier Diaz <coderdiaz@gmail.com>
2022-10-05 12:47:45 -0500

    feat: added support for validate cookie from request

--- src/pages/api/yelp.tsx
+++ src/pages/api/yelp.tsx
@@ -1,5 +1,6 @@
-import http from 'http';
+import { NextApiRequest, NextApiResponse } from 'next';
 import httpProxy from 'http-proxy';
+import Cookies from 'cookies';
 
 const API_URL = process.env.YELP_API_URL;
 const proxy = httpProxy.createProxyServer();
@@ -9,7 +10,7 @@ export const config = {
   api: { bodyParser: false },
 };
 
-export default (req: http.IncomingMessage, res: http.ServerResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   return new Promise((resolve, reject) => {
     // Rewrite the URL: strip out the leading '/api/yelp'.
     // For example, '/api/yelp' would become '/'.
@@ -18,7 +19,22 @@ export default (req: http.IncomingMessage, res: http.ServerResponse) => {
 
     // Adding headers for access to Yelp API GraphQL
     req.headers['Authorization'] = `Bearer ${process.env.YELP_API_KEY || ''}`;
-    req.headers['Accept-Language'] = 'en_US';
+
+    // Getting cookie from the request
+    const cookies = new Cookies(req, res);
+    const isAllowed = cookies.get('x-allowed');
+
+    if (!isAllowed) {
+      return res.status(401).json({
+        error: {
+          code: 'NOT_ALLOWED_REQUEST',
+          description: 'Only authorized domains are allowed to request.',
+        },
+      });
+    }
+
+    // Don't forward cookies to API
+    req.headers.cookie = '';
 
     // Proxy logic for resolve requests
     proxy
