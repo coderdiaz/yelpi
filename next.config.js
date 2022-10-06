0033801b8f4ccc8e6636db2b8a522e2fd75f4adb
Javier Diaz <coderdiaz@gmail.com>
2022-10-06 01:07:31 -0500

    chore: updated allowed images domains

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@
   images: {
     domains: [
       's3-media1.fl.yelpcdn.com',
+      's3-media2.fl.yelpcdn.com',
+      's3-media3.fl.yelpcdn.com',
+      's3-media4.fl.yelpcdn.com',
     ],
   },
  };
