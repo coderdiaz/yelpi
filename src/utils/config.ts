78524f71685d8a6a4f7fedc45d8c05829b3651e4
Javier Diaz <coderdiaz@gmail.com>
2022-10-06 20:52:23 -0500

    fix: changed cities values

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,18 +1,18 @@
-type City = {
+export type LatLng = {
   latitude: number;
   longitude: number;
 }
 
 type Config = {
   cities: {
-    [key: string]: City;
+    [key: string]: LatLng;
   };
 };
 
-const config: Config = {
+export const config: Config = {
   cities: {
     cdmx: { latitude: 19.44216, longitude: -99.156579 },
-    mty: { latitude: 19.44216, longitude: -99.156579 },
-    gdl: { latitude: 19.44216, longitude: -99.156579 },
+    mty: { latitude: 25.6753607, longitude: -100.3477979 },
+    gdl: { latitude: 20.6737883, longitude: -103.3704326 },
   },
 };
