88b8de01df4a6cfd02f07accfba29a34f06dafe8
Javier Diaz <coderdiaz@gmail.com>
2022-10-08 17:20:44 -0500

    feat: added embed page component

---
+++ src/components/EmbedMap.tsx
@@ -0,0 +1,33 @@
+import { styled } from "@stitches/config";
+
+type EmbedMapProps = {
+  lat: number;
+  lng: number;
+};
+
+export default function EmbedMap({
+  lat,
+  lng
+}: EmbedMapProps) {
+  return (
+    <StyledMapWrapper className="w-full h-full relative">
+      <StyledEmbedMap
+        dangerouslySetInnerHTML={{ 
+          __html: `<div class="mapouter">
+          <div class="gmap_canvas"><iframe style="width: 100%;height: 240px" id="gmap_canvas" src="https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}&q=${lat},${lng}&zoom=18" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://coderdiaz.dev"></a><br><style>.mapouter{position:relative;text-align:right;height: 240px;width:100%;}</style><style>.gmap_canvas {overflow:hidden;background:none!important;height: 240px;width:100%;}.place-card{display: none;}</style></div></div>`
+        }} />
+    </StyledMapWrapper>
+  );
+}
+
+const StyledMapWrapper = styled('div', {
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+});
+
+const StyledEmbedMap = styled('div', {
+  position: 'relative',
+  borderRadius: 12,
+  overflow: 'hidden',
+});
