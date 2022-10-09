55c447e9646d7d3fda63d98aeea0e4d4fed32a62
Javier Diaz <coderdiaz@gmail.com>
2022-10-08 19:39:11 -0500

    chore: added new allowed image domain for unsplash

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
  module.exports = {
   images: {
     domains: [
+      'images.unsplash.com',
       's3-media1.fl.yelpcdn.com',
       's3-media2.fl.yelpcdn.com',
       's3-media3.fl.yelpcdn.com',
