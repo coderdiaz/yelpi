04f98f8f91db6ef2662a039792c9b916fba64714
Javier Diaz <coderdiaz@gmail.com>
2022-10-10 09:27:26 -0500

    feat: added seo cover image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Yelpi — Restaurants, Dentists, Bars, Beauty Salons, Doctors</title>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <meta property="og:title" content="Yelpi — Restaurants, Dentists, Bars, Beauty Salons, Doctors" />
+        <meta property="og:description" content="Easily find placeds to have breakfast, lunch or dinner from our partners or local businesses" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://yelpi.coderdiaz.dev/cover.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@coderdiaz" />
+        <meta name="twitter:title" content="Yelpi — Restaurants, Dentists, Bars, Beauty Salons, Doctors" />
+        <meta name="twitter:description" content="Easily find placeds to have breakfast, lunch or dinner from our partners or local businesses" />
+        <meta name="twitter:image" content="https://yelpi.coderdiaz.dev/cover.png" />
       </Head>
       <Component {...pageProps} />
     </>
